702cbc58e626550d9c4a5be585e8ae448aa3c918
Mat Geist <mgeist@betterworks.com>
2015-08-16 16:15:27 -0700

    no longer hotel california

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import logout as auth_logout
+from django.shortcuts import redirect, render
 
 
 def index(request):
-  print '!!!!!!!!!!!!!!!!!!!'
-  print request
-  print request.user
-  print '~~~~~~~~~~~~~~~~~~~'
   return render(request, 'index.html')
+
+def logout(request):
+  auth_logout(request)
+  return redirect('/')
